639570eddf51a0df705e9500389e635d76312e6c
Raghavendiran Nagarajan <sriragav.2009@gmail.com>
2021-04-28 14:08:40 -0700

    Oracle Python package setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='oracle_loadtest',
+	version='1.0',
+	author='Raghav Nagarajan',
+	author_email='sriragav.2009@gmail.com',
+	packages=find_packages(),
+)
