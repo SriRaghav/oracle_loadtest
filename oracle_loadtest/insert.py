59dca225129b183bd463e955ee0ed92e67e02161
Raghavendiran Nagarajan <raghavendiran@google.com>
2021-06-09 18:38:20 +0000

    SF + Oracle changes

--- oracle_loadtest/insert.py
+++ oracle_loadtest/insert.py
@@ -2,17 +2,35 @@ import cx_Oracle
 import names
 import random
 import sys
+import json
+import argparse
 from queue import Queue
 
 
 class OracleLoadTest:
 
-    def __init__(self, user, password, hostname, service_id, port_number):
-        self.user = user
-        self.password = password
-        self.hostname = hostname
-        self.service_id = service_id
-        self.port_number = port_number
+    file_path = "/home/oracle_loadtest/oracle_cred.txt"
+
+    def __init__(self):
+        self.user = None
+        self.password = None
+        self.hostname = None
+        self.service_id = None
+        self.port_number = None
+        self.table_name = None
+        self.operation = None
+        self.num_rows = 0
+        self.is_mockup = False
+
+    def read_creds_from_file(self):
+
+        with open(self.file_path, "r") as reader:
+            oracle_cred_json = json.loads(reader.read())
+
+        self.user = oracle_cred_json["user"]
+        self.password = oracle_cred_json["password"]
+        self.service_id = oracle_cred_json["service_id"]
+        self.port_number = oracle_cred_json["port_number"]
 
     @staticmethod
     def generate_namelist(number_records):
@@ -57,7 +75,12 @@ class OracleLoadTest:
                                    self.password,
                                    self.hostname + '/' + self.service_id) as connection_obj:
                 with connection_obj.cursor() as cursor_obj:
-                    query_builder = "select " + column + " from " + table_name
+
+                    if column == "ALL":
+                        query_builder = "select " + column + " from " + table_name
+                    else:
+                        query_builder = "select " + column + " from " + table_name + " order by " + column + " asc"
+
                     cursor_obj.execute(query_builder)
 
                     return [record[0] for record in cursor_obj.fetchall()]
@@ -65,7 +88,25 @@ class OracleLoadTest:
             print("\nTable - %s & # FETCHED records - %s " % (table_name, str(cursor_obj.rowcount)))
 
         except cx_Oracle.DatabaseError as e:
-            print("Oracle DB Error!", e)
+            print("Oracle DB Error! - Select function", e)
+
+    def describe(self, table_name):
+
+        try:
+            with cx_Oracle.connect(self.user,
+                                   self.password,
+                                   self.hostname + '/' + self.service_id) as connection_obj:
+                with connection_obj.cursor() as cursor_obj:
+
+                    query_builder = "select * from " + table_name + " ROWNUM 1"
+
+                    cursor_obj.execute(query_builder)
+
+                    print(cursor_obj.description)
+                    return [record[0] for record in cursor_obj.description()]
+
+        except cx_Oracle.DatabaseError as e:
+            print("Oracle DB Error - Describe function!", e)
 
     def update(self, table_name, column, values, update_many=True):
 
@@ -109,11 +150,16 @@ class OracleLoadTest:
             print("Oracle DB Error!", e)
 
 
-def main(query, tables, number_records):
-    olt = OracleLoadTest('striim', 'oracle', 'localhost', 'xe', 1521)
-    table_list = tables.split(",")
+def main(olt):
 
-    if query == "insert":
+    if olt.operation == "insert":
+        records = olt.select(olt.table_name, "SPEC_ID")
+        print(records[1:2])
+
+    if olt.operation == "describe":
+        print("In Describe")
+
+    '''if olt.opertion == "insert":
 
         namelist = olt.generate_namelist(number_records)
         id_list = olt.generate_numlist(number_records, 2, 20000)
@@ -145,35 +191,52 @@ def main(query, tables, number_records):
         if order_table_name is not None:
             olt.insert(order_table_name, "CUSTOMER_ID, ORDER_VALUE", order_records)
             print("Sample Records below:(CUSTOMER_ID, ORDER_VALUE)")
-            print(order_records[:2])
+            print(order_records[:2])'''
 
-    if query == "update":
+    if olt.operation == "update":
 
-        records = olt.select(tables, "CUSTOMER_ID")
+        records = olt.select(olt.table_name, "CUSTOMER_ID")
 
-        if len(records) >= number_records:
-            old_values = random.sample(records, number_records)
-            new_values = olt.generate_numlist(number_records, 10, 100000)
+        if len(records) >= olt.num_rows:
+            old_values = random.sample(records, olt.num_rows)
+            new_values = olt.generate_numlist(olt.num_rows, 10, 100000)
             update_values = [(old, new) for old, new in zip(old_values, new_values)]
             print(update_values)
-            olt.update(tables, "CUSTOMER_ID", update_values)
+            olt.update(olt.table_name, "CUSTOMER_ID", update_values)
 
-    if query == "delete":
+    if olt.operation == "delete":
 
-        records = olt.select(tables, "CUSTOMER_ID")
+        records = olt.select(olt.table_name, "CUSTOMER_ID")
 
-        if len(records) >= number_records:
-            old_vaules = random.sample(records, number_records)
+        if len(records) >= olt.num_rows:
+            old_vaules = random.sample(records, olt.num_rows)
             update_values = [(old,) for old in old_vaules]
             print(update_values)
-            olt.delete(tables, "CUSTOMER_ID", update_values)
+            olt.delete(olt.table_name, "CUSTOMER_ID", update_values)
         else:
             print("Error: # Records in the table is " + str(len(records)))
 
 
 if __name__ == "__main__":
 
-    if len(sys.argv) == 4:
-        main(sys.argv[1], sys.argv[2], int(sys.argv[3]))
-    else:
-        print("Wrong number of inputs, exiting now!")
+    olt = OracleLoadTest()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cred_from_file", action="store_true", dest="cred_from_file")
+    parser.add_argument("--user", type=str, dest="user")
+    parser.add_argument("--password", type=str, dest="password")
+    parser.add_argument("--hostname", type=str, dest="hostname")
+    parser.add_argument("--service_id", type=str, dest="service_id")
+    parser.add_argument("--port_number", type=str, dest="port_number")
+    parser.add_argument("--table_name", type=str, dest="table_name")
+    parser.add_argument("--operation", type=str, choices=["insert", "update", "delete", "describe"], dest="operation",
+                        required=True)
+    parser.add_argument("--is_mockup", action="store_true", dest="is_mockup")
+    parser.add_argument("--num_rows", type=int, dest="num_rows", required=True)
+
+    args = parser.parse_args(namespace=olt)
+
+    if args.cred_from_file:
+        olt.read_creds_from_file()
+
+    main(olt)
